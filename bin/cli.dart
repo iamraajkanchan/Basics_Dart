b9c6d3d8e6999965ceb834c2da967813069ed127
Raj Kanchan <iamraajkanchan@gmail.com>
2023-10-20 11:24:13 +0530

    Example of List, Set and Map is added in collections.dart

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,7 +1,15 @@
-
 import '../lib/collections/collections.dart';
 
 void main(List<String> arguments) {
+  print("Showing Super Heroes");
   addSuperHeroes();
   showSuperHeroes();
+  print("");
+  print("Showing Super Villains");
+  addSuperVillain();
+  showSuperVillains();
+  print("");
+  print("Showing Hero Villain Group");
+  addHeroVillainGroup();
+  showHeroVillainGroup();
 }
--- lib/collections/collections.dart
+++ lib/collections/collections.dart
@@ -1,4 +1,6 @@
+// superHeroes is a list.
 final superHeroes = <String>[];
+
 void addSuperHeroes() {
   superHeroes.add("Spider Man");
   superHeroes.add("Iron Man");
@@ -7,13 +9,47 @@ void addSuperHeroes() {
   superHeroes.add("Wonder Woman");
   superHeroes.add("Hulk");
 }
+
 void showSuperHeroes() {
   int i = 1;
-  for(String name in superHeroes) {
+  for (String name in superHeroes) {
     print("Super Hero $i : $name");
     i++;
-    if(i > superHeroes.length) {
-      break;
-    }
+  }
+}
+
+// superVillain is a set
+final superVillain = <String>{};
+
+void addSuperVillain() {
+  superVillain.add("Lex Luther");
+  superVillain.add("Thanos");
+  superVillain.add("Skeletor");
+  superVillain.add("Doctor Doom");
+  superVillain.add("Skeletor");
+  superVillain.add("Magneto");
+}
+
+void showSuperVillains() {
+  int i = 1;
+  for (String name in superVillain) {
+    print("Super Villain $i : $name");
+    i++;
+  }
+}
+
+// heroVillainGroup is a Map
+final heroVillainGroup = Map<String, String>();
+void addHeroVillainGroup() {
+  heroVillainGroup[superHeroes.firstWhere((element) => element == "Super Man")] = superVillain.firstWhere((element) => element == "Lex Luther");
+  heroVillainGroup[superHeroes.firstWhere((element) => element == "Iron Man")] = superVillain.firstWhere((element) => element == "Thanos");
+  heroVillainGroup[superHeroes.firstWhere((element) => element == "He Man")] = superVillain.firstWhere((element) => element == "Skeletor");
+  heroVillainGroup[superHeroes.firstWhere((element) => element == "Wonder Woman")] = superVillain.firstWhere((element) => element == "Magneto");
+  heroVillainGroup[superHeroes.firstWhere((element) => element == "Hulk")] = superVillain.firstWhere((element) => element == "Doctor Doom");
+}
+
+void showHeroVillainGroup() {
+  for(String key in heroVillainGroup.keys) {
+    print("$key : ${heroVillainGroup[key]}");
   }
 }
